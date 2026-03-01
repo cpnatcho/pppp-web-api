6639af5e232e4cf9f74af58c36befa259cde1b66
10christoph <christoph.prasser2@stud.htw-dresden.de>
2026-03-01 16:25:39 +0100

    feat: Add endpoint /currentdate

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func currentDate() string {
 	return time.Now().Format("2006-01-02")
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/currentdate", func(c *gin.Context) {
+		currentDate := currentDate()
+
+		c.JSON(http.StatusOK, gin.H{
+			"date": currentDate,
+		})
+	})
+
+	// Start server on port 8080 (default)
+	if err := r.Run(); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
 }
